c969d4c753fb44ff52b6fdd5d49d37da46b317ba
Chelsea Long <clong6@gmail.com>
2019-02-12 23:39:21 -0700

    saving project

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,66 +1,36 @@
 module.exports = [
-{
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-    5,
-    1,
-    4,
-    4,
-    5,
-    1,
-    2,
-    2,
-    2,
-    2
-    ]
-},
-{
-    "name": "Bahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        3,
-        3,
-        3
-    ]
-    },
-    {
-    "name": "Cahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        1,
-        1,
-        1
-    ]
-    },
-    {
-    "name": "Dahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        5,
-        5
-    ]
-    }
-]
+  {
+    name: "Rachel",
+    photo:
+      "https://pbs.twimg.com/profile_images/578900195812900864/r1t12Hyb.jpeg",
+    scores: [1, 1, 2, 2, 1, 1, 2, 2, 2, 2]
+  },
+  {
+    name: "Ross",
+    photo: "http://cultbox.co.uk/wp-content/uploads/2018/08/ross-geller.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 3, 3, 3]
+  },
+  {
+    name: "Monica",
+    photo:
+      "https://s3.r29static.com//bin/entry/229/0,0,2000,2400/720x864,85/1604388/image.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 1, 1, 1]
+  },
+  {
+    name: "Pheobe",
+    photo:
+      "https://media1.popsugar-assets.com/files/thumbor/L6u2EA_RHtV_piy8CDItMVqiUQ4/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2014/11/04/740/n/1922283/b873d68328a0eef2_Phoebe-Top.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 5, 5]
+  },
+  {
+    name: "Chandler",
+    photo: "https://thebigfatlist.com/images/options/chandler-friends.jpg",
+    scores: [5, 5, 4, 4, 5, 5, 4, 5, 5, 5]
+  },
+  {
+    name: "Joey",
+    photo:
+      "https://www.worldnewslive4you.com/wp-content/uploads/2019/01/nintchdbpict000003032187-e1523634380964.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 5, 5]
+  }
+];
